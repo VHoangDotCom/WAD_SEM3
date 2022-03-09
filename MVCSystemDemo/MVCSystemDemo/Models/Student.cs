bd1ac6347597c2826c446926d12d1a80d1aac279
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-03-09 18:06:47 +0700

    About Page

--- MVCSystemDemo/MVCSystemDemo/Models/Student.cs
+++ MVCSystemDemo/MVCSystemDemo/Models/Student.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MVCSystemDemo.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +9,36 @@ namespace MVCSystemDemo.Models
 {
     public class Student
     {
+        //ID
         public int ID { get; set; }
+
+        //LastName
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string LastName { get; set; }
+
+        //Fisrt name
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string FirstMidName { get; set; }
+
+        //Enrollment date
+        [Required]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(StudentViewModel stview)
+        {
+            this.LastName = stview.LastName;
+            this.FirstMidName = stview.FirstMidName;
+            this.EnrollmentDate = stview.EnrollmentDate;
+           
+        }
     }
 }
